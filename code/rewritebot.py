34ac6778b385bc260cdaa7d704d4cf63b60d5f65
LazyNeko1 <48885326+LazyNeko1@users.noreply.github.com>
2019-10-04 07:34:58 -0700

    Update rewritebot.py

--- code/rewritebot.py
+++ code/rewritebot.py
@@ -1,7 +1,11 @@
 from discord.ext import commands
 prefix = "#="
+import nbapi
 bot = commands.Bot(command_prefix=prefix)
 
 #i have no idea how this works I'll fix it later today 
 
-bot.run(TOKEN)
+@bot.command()
+async def neko(ctx):
+    
+    await ctx.send(nbapi.random.neko())#i have no idea how to make a embed 
