3fcd36793cc0054e3e8d4c59e0503f38f98a6937
LazyNeko1 <48885326+LazyNeko1@users.noreply.github.com>
2019-10-04 10:56:49 -0700

    Update bot.js

--- code/bot.js
+++ code/bot.js
@@ -1,1 +1,43 @@
-//is not yet made. Still need a api using js 
+//is not yet made. Still need a api using js
+
+//Right now, i'm just going to use the raw API data. /shrug
+//obviously requires the dependincies below
+const Discord = require('discord.js')
+
+
+//end of dependincies
+const client = new Discord.Client()
+
+
+
+
+
+//this may not work, im still testing it
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', (receivedMessage) => {
+    // Prevent bot from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+
+const nekoEmbed = new Discord.RichEmbed()
+	.setTitle('Nekos!')
+	.setImage(nbapi.random.neko())
+	.setFooter('Made by LazyNeko', 'http://neko-bot.net/images/ownerimg.round.png');
+    receivedMessage.channel.send(nekoEmbed)
+})
+
+
+
+client.login('')
