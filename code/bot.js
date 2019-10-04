76411a0c8fee297b2637115b79c79d96ab53272a
LazyNeko1 <48885326+LazyNeko1@users.noreply.github.com>
2019-10-04 11:01:37 -0700

    Update bot.js

--- code/bot.js
+++ code/bot.js
@@ -3,7 +3,7 @@
 //Right now, i'm just going to use the raw API data. /shrug
 //obviously requires the dependincies below
 const Discord = require('discord.js')
-
+const nbapi = require('nbapi')
 
 //end of dependincies
 const client = new Discord.Client()
@@ -33,7 +33,7 @@ client.on('message', (receivedMessage) => {
 
 const nekoEmbed = new Discord.RichEmbed()
 	.setTitle('Nekos!')
-	.setImage(nbapi.random.neko())
+	.setImage(nbapi.randomNeko())
 	.setFooter('Made by LazyNeko', 'http://neko-bot.net/images/ownerimg.round.png');
     receivedMessage.channel.send(nekoEmbed)
 })
