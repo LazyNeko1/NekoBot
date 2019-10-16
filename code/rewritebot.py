db5301322f82a894b311b11066f516a916a53c23
LazyNeko1 <48885326+LazyNeko1@users.noreply.github.com>
2019-10-16 11:57:52 -0700

    Updated bot, added pats,and anime command.

--- code/rewritebot.py
+++ code/rewritebot.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
 import discord
 
-prefix = "="
+prefix = "#="
 
 import nbapi
+from random import randint
 
 bot = commands.Bot(command_prefix=prefix)
 
@@ -21,7 +22,27 @@ async def neko(ctx):
     embed=discord.Embed(title='Nekos!')
     embed.set_image(url=nbapi.random.neko())
     await ctx.send(embed=embed)#i have no idea how to make a embed 
+@bot.command()
+
+async def anime(ctx):
+
+    
+    embed=discord.Embed(title='Anime!')
+    embed.set_image(url=nbapi.random.anime())
+    await ctx.send(embed=embed)#i have no idea how to make a embed 
+@bot.command()
+
+async def pat(ctx):
+
+    
+    embed=discord.Embed(title='Pats!')
+    num = randint(0,1)
+    if num == 0:
+        embed.set_image(url=nbapi.random.pat.static())
+    if num == 1:
+        embed.set_image(url=nbapi.random.pat.animated())
+    await ctx.send(embed=embed)#i have no idea how to make a embed 
 
     
     
-    bot.run('token')
+bot.run('token')
